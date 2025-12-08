d715f25d8ddd63273430a476c072e952a98e1f0c
MotazAboAssi <motazaboassi@gmail.com>
2025-12-08 14:27:10 +0300

    first commit

---
+++ HW2/Cube.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef CUBE_H
+#define CUBE_H
+
+#include <vector>
+#include <glm/glm.hpp>
+#include <learnopengl/shader_m.h>
+#include "Polygon.h"
+
+class Cube {
+public:
+	Cube();
+
+	Cube(glm::vec3 center, float edge, glm::vec3 color);
+
+	void transformation(glm::mat4 t);
+
+	void draw(Shader& shader);
+	glm::mat4 getMatrix();
+	glm::vec3 getCenter();
+
+private:
+	glm::vec3 center;
+	float edge;
+	glm::vec3 color;
+	glm::mat4 model;
+	std::vector<Polygon> faces;
+};
+
+#endif // CUBE_H
